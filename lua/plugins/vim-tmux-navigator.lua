84eadc64a3ce924e58364b0d0faa2ae064da9b60
Alex Lapins <alex@lapins.com>
2026-01-19 15:18:18 -0500

    fix keymaps for when not running in tmux

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -9,10 +9,19 @@ return {
     "TmuxNavigatorProcessList",
   },
   keys = {
-    { "<M-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<M-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<M-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<M-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<M-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    { mode = "n", "<M-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { mode = "n", "<M-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { mode = "n", "<M-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { mode = "n", "<M-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { mode = "n", "<M-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+    { mode = "t", "<M-h>", "<C-w><cmd>TmuxNavigateLeft<cr>" },
+    { mode = "t", "<M-j>", "<C-w><cmd>TmuxNavigateDown<cr>" },
+    { mode = "t", "<M-k>", "<C-w><cmd>TmuxNavigateUp<cr>" },
+    { mode = "t", "<M-l>", "<C-w><cmd>TmuxNavigateRight<cr>" },
+    { mode = "t", "<M-\\>", "<C-w><cmd>TmuxNavigatePrevious<cr>" },
   },
+  init = function()
+    -- Disable default mappings to make it work in terminal mode
+    vim.g.tmux_navigator_no_mappings = 1
+  end,
 }
